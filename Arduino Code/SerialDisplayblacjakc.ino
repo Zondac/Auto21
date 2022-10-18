1301ba42acdbde380696960ebe7848182f092b49
Zondac <odd-mar10n@hotmail.com>
2022-10-18 13:52:03 +0200

    Game logic python script
    Includes placeholder functions for drawing cards, generated thus not taken from the card recognizer, the players abilities to hit, stand and double down, as well as score comparing, bust logic and dealer logic

---
+++ Arduino Code/SerialDisplayblacjakc.ino
@@ -0,0 +1,92 @@
+/**
+ * Displays text sent over the serial port (e.g. from the Serial Monitor) on
+ * an attached LCD.
+ */
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+
+LiquidCrystal_I2C lcd[4]={
+  LiquidCrystal_I2C(0x27,16,2),
+  LiquidCrystal_I2C(0x26,16,2),
+  LiquidCrystal_I2C(0x25,16,2),
+  LiquidCrystal_I2C(0x3F,16,2)
+};
+String inputBuffer;
+int p1btn1 = 1;
+int p1btn2 = 1;
+int p1btn3 = 1;
+
+void setup()
+{
+  for (int i = 0; i<4; i++){
+    lcd[i].begin();
+    lcd[i].backlight();
+    lcd[i].clear();
+    lcd[i].setCursor(0,0);
+    lcd[i].print("Chips:");
+    lcd[i].setCursor(0,1);
+    lcd[i].noCursor();
+  }
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  
+	// Initialize the serial port at a speed of 9600 baud
+	Serial.begin(9600);
+}
+
+  
+void loop()
+{
+  int p1btn1prev = p1btn1;
+  int p1btn2prev = p1btn2;
+  int p1btn3prev = p1btn3;
+  p1btn1 = digitalRead(3);
+  p1btn2 = digitalRead(4);
+  p1btn3 = digitalRead(2);
+  if (p1btn1==LOW && p1btn1prev ==HIGH) Serial.println("p1.A");
+  if (p1btn2==LOW && p1btn1prev ==HIGH) Serial.println("p1.B");
+  if (p1btn3==LOW && p1btn1prev ==HIGH) Serial.println("p1.C");
+	if (Serial.available()) {
+		// Wait a bit for the entire message to arrive
+		// Clear the screen
+   for (int i = 0; i<4; i++){
+    lcd[i].setCursor(0,1);
+   }
+   
+		// Write all characters received with the serial port to the LCD.
+		while (Serial.available() > 0) {
+      inputBuffer = Serial.readString();
+      }
+      switch (inputBuffer[0]){
+        case 'q':
+        
+        
+          inputBuffer.remove(0,1);
+          lcd[0].print("                ");
+          lcd[0].setCursor(0,1);
+          lcd[0].print(inputBuffer);
+        break;
+        
+        case 'w':
+        
+        inputBuffer.remove(0,1);
+          lcd[1].print(inputBuffer);
+        break;
+        
+        case 'e':
+        
+        inputBuffer.remove(0,1);
+          lcd[2].print(inputBuffer);
+        break;
+        
+        case 'r':
+        
+        inputBuffer.remove(0,1);
+          lcd[3].print(inputBuffer);
+        break;
+      }
+	}
+}
---
+++ Arduino Code/autojack.py
@@ -0,0 +1,114 @@
+import random
+
+def main():
+    deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+
+    def draw_card():
+       draw = random.choice(deck)
+       return draw
+    def handVal(player_hand):
+        player_hand.sort()
+        valSum = 0
+        for x in range(len(player_hand)):
+            
+            if int(player_hand[x]) == 14:
+                valSum += 11
+                if valSum > 21:
+                    valSum -= 10
+            
+            elif (int(player_hand[x]) > 10):
+                valSum += 10
+            else:
+                valSum += int(player_hand[x])
+        
+        if (valSum > 21):
+            #bust case
+            return 0
+        
+        return valSum
+
+    def printHand(playerID, pHand):
+        print("Player ", playerID, " has: ", pHand, " to a total value of: ", handVal(pHand))
+
+    def playerchoice(playerID, playerhand):
+        finished = False
+        while (finished == False):
+            printHand(playerID, playerhand)
+            if (handVal(playerhand) == 21):
+                print("Blackjack!")
+                finished = True
+
+            if (finished == False):
+                pinput = input ("H to hit, S to stand, D to Double Down\n")
+
+                if pinput == "h":
+                    playerhand.append(draw_card()) 
+
+                    if (handVal(playerhand) == 0):
+                        printHand(playerID, playerhand)
+                        print("You busted!")
+
+                        finished = True
+                    elif(handVal(playerhand) == 21):
+                        printHand(playerID, playerhand)
+                        print ("You have blacjack!")
+                        finished = True
+
+                elif pinput == "d":
+                    playerhand.append(draw_card())
+                    printHand(playerID, playerhand)
+                    finished = True
+                    if (handVal(playerhand) == 0):
+                        print("You busted!")
+
+                elif pinput == "s":
+                    finished = True
+
+    def play_deck():
+        drawncards = [draw_card(), draw_card()]
+        return drawncards
+
+    def dealerchoice():
+        if (handVal(players[4]) == 0):
+            print("Dealer busted!")
+            return
+
+        elif(handVal(players[4]) < 16):
+            players[4].append(draw_card())
+            dealerchoice()
+
+        else:
+            return
+
+    def scorecompare(playerID,playerHand):
+        if(handVal(playerHand) > handVal(players[4])):
+            print("Player ",playerID , "has a score of ", handVal(playerHand)," and beat the dealer"))
+        elif (handVal(playerHand) == handVal(players[4])):
+            print("Player ",playerID ," drew")
+        else:
+            print("Dealer beat player ", playerID)
+
+    #Initiate hand arrays
+    dealerhand = []
+    player1hand = []
+    player2hand = []
+    player3hand = []
+    player4hand = []
+    players = [player1hand, player2hand, player3hand, player4hand, dealerhand]
+    rounds = 1
+
+    for x in range(rounds):
+        for y in range(len(players)):
+            players[y] = play_deck()
+
+        for y in range(len(players)-1):
+            playerchoice(y+1,players[y])
+
+        dealerchoice()
+
+        printHand("Dealer", players[4])
+
+        for y in range(len(players)-1):
+            scorecompare(y+1,players[y])
+
+main()
