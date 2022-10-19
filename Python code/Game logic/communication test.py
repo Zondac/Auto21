6692a21c3448129a8117ee23f8db673a92e31ea7
Hokij <113107110+Hokij@users.noreply.github.com>
2022-10-19 10:35:22 +0200

    Add files via upload

---
+++ Python code/Game logic/communication test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 08:34:36 2022
+
+@author: Windows User
+"""
+
+import serial  
+import time
+arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+while True:
+    num = input("Choose how many players 1, 2, 3 or 4. \n ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
