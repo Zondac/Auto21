a504596d75d07e3f755b680df836b7c386a15a57
zondac <odd-mar10n@hotmail.com>
2022-11-22 13:09:01 +0100

    format

--- Arduino Code/Motors code/stepper_test.ino
+++ Arduino Code/Motors code/stepper_test.ino
@@ -1,5 +1,5 @@
 #include <Stepper.h>
-#include <Wire.h> 
+#include <Wire.h>
 #include "controllers.h"
 #include <LiquidCrystal_I2C.h>
 
@@ -7,28 +7,27 @@ int inputint;
 String inputraw;
 char output;
 bool readysignal;
+char inputchar;
 // Stepper settings
 // Defines the number of steps per rotation
-const int stepsPerRevolution = 1892; //473 steps for 10 v for 1/4 circle
+const int stepsPerRevolution = 1892; // 473 steps for 10 v for 1/4 circle
 const int twentyPerRevolution = 480;
 int currentLocation = 0;
 
-int locationarray[5]  {twentyPerRevolution, twentyPerRevolution*3,twentyPerRevolution*4,twentyPerRevolution*5,twentyPerRevolution*7};
+int locationarray[5]{twentyPerRevolution, twentyPerRevolution * 3, twentyPerRevolution * 4, twentyPerRevolution * 5, twentyPerRevolution * 7};
 
 Controller player1(22);
 Controller player2(25);
 Controller player3(28);
 Controller player4(31);
 
-LiquidCrystal_I2C lcd[4]={
-  LiquidCrystal_I2C(0x27,16,2),
-  LiquidCrystal_I2C(0x26,16,2),
-  LiquidCrystal_I2C(0x25,16,2),
-  LiquidCrystal_I2C(0x3F,16,2)
-};
+LiquidCrystal_I2C lcd[4] = {
+    LiquidCrystal_I2C(0x27, 16, 2),
+    LiquidCrystal_I2C(0x26, 16, 2),
+    LiquidCrystal_I2C(0x25, 16, 2),
+    LiquidCrystal_I2C(0x3F, 16, 2)};
 
-
-//Stepper motor
+// Stepper motor
 #define IN1 9
 #define IN2 8
 #define IN3 7
@@ -39,7 +38,7 @@ int ena = 10;
 int in1 = 11;
 int in2 = 12;
 
-//Numeric variables
+// Numeric variables
 int speed1 = 15;
 int speed2 = 30;
 int delaytime = 250;
@@ -48,11 +47,12 @@ int delaytime = 250;
 // Pins entered in sequence IN1-IN2 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, IN1, IN2);
 
-void setup() {
+void setup()
+{
   // python communication
   Serial.begin(115200);
   Serial.setTimeout(1);
-  
+
   // Dc MOTOR
   pinMode(ena, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -60,125 +60,134 @@ void setup() {
 
   for (size_t i = 0; i < 4; i++)
   {
-  setScore(lcd[0], 500);
-  }
-  
-  {
-    lcd[i].begin(16,2);
+    lcd[i].begin(16, 2);
     lcd[i].backlight();
     lcd[i].clear();
-    lcd[i].setCursor(0,0);
+    lcd[i].setCursor(0, 0);
     lcd[i].print("Chips:");
-    lcd[i].setCursor(0,1);
+    lcd[i].setCursor(0, 1);
     lcd[i].noCursor();
+    setScore(lcd[i], 500);
   }
 
+  blackjackBegin();
 }
 
 void loop()
 {
-  //Initial Deal
-  blackjackBegin();
+  while (Serial.available() > 0){
+    inputraw = Serial.readString();
+    inputint = inputraw.toInt();
+    inputchar = inputraw.charAt(0);
 
-  readysignal = false; 
+    switch (inputchar)
+    {
+    case 'd':
+      output = player1.getPlayerInput();
+      break;
+    case 'e':
+      output = player2.getPlayerInput();
+      break;
+    case 'f':
+      output = player3.getPlayerInput();
+      break;
+    case 'g':
+      output = player4.getPlayerInput();
+      break;
+    case 'p':
+      DcMotor();
+      break;
+    case 'h':
+      resetLocation();
+      break;
+    case 's':
+      resetLocation();
+      blackjackBegin();
+      break;
+    case 'i':
+      setScore(lcd[0], inputint);
+      break;
+    case 'j':
+      setScore(lcd[1], inputint);
+      break;
+    case 'k':
+      setScore(lcd[2], inputint);
+      break;
+    case 'l':
+      setScore(lcd[3], inputint);
+      break;
+    default:
+      break;
+    }
 
-
-  //Players start choosing
-  while(!readysignal){
-  while(!Serial.available());
-  inputraw = Serial.readString();
-  inputint = inputraw.toInt();
-  char inputchar = inputraw.charAt(0);
-  
-  switch (inputchar)
-  {
-  case 'd':
-    output = player1.getPlayerInput();
-    break;
-  case 'e':
-    output = player2.getPlayerInput();
-    break;
-  case 'f':
-    output = player3.getPlayerInput();
-    break;
-  case 'g':
-    output = player4.getPlayerInput();
-    break;
-  case 'p':
-    DcMotor();
-    break;
-  case 'h':
-    readysignal = !readysignal;
-    break;
-  default:
-    break;
+    Serial.write(output);
   }
-
-  Serial.write(output);
-  }
-
-resetLocation()
-} 
-
-
-void  MoveToPlayer(int player) {
-  myStepper.setSpeed(speed1);
-  myStepper.step(locationarray[player-1]-currentLocation);
-  currentLocation = locationarray[player-1];
 }
 
-void DcMotor() {
-  //DC MOTOR
-   //CLOCKWISE MAX SPEED
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
+void MoveToPlayer(int player)
+{
+  myStepper.setSpeed(speed1);
+  myStepper.step(locationarray[player - 1] - currentLocation);
+  currentLocation = locationarray[player - 1];
+}
+
+void DcMotor()
+{
+  // DC MOTOR
+  // CLOCKWISE MAX SPEED
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
   digitalWrite(ena, HIGH);
   delay(95);
-   digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
   delay(20);
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
   digitalWrite(ena, HIGH);
   delay(50);
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
+  // STOP
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
   delay(50);
 }
 
-void initialDeal(int playeramount){
+void initialDeal(int playeramount)
+{
   for (size_t y = 0; y < 2; y++)
   {
-  
-  for (size_t i = 1; i <= playeramount; i++)
+
+    for (size_t i = 1; i <= playeramount; i++)
     {
       MoveToPlayer(i);
-      while(!Serial.available());
+      while (!Serial.available())
+        ;
       Serial.read();
       DcMotor();
     }
   }
 }
 
-
-void setScore(LiquidCrystal_I2C screen, int score){          
+void setScore(LiquidCrystal_I2C screen, int score)
+{
   screen.print("                ");
-  screen.setCursor(0,1);
+  screen.setCursor(0, 1);
   screen.print(inputBuffer);
-  screen.setCursor(0,1);
+  screen.setCursor(0, 1);
 }
 
-void resetLocation(){
+void resetLocation()
+{
   myStepper.step(-currentLocation);
   currentLocation = 0;
 }
 
-void blackjackBegin(){
-  
-  while (!Serial.available());
+void blackjackBegin()
+{
+  while (!Serial.available())
+    ;
   inputraw = Serial.readString();
   inputint = inputraw.toInt();
-  initialDeal(i+nputint1);
+  initialDeal(i + nputint1);
   Serial.read();
 }
