38acc62d5276c47f095ca14891a5bfb88524aa78
Nikolai-E <115153285+Nikolai-E@users.noreply.github.com>
2022-11-28 13:13:10 +0100

    Add files via upload

--- Python code/Game logic/autojack.py
+++ Python code/Game logic/autojack.py
@@ -2,6 +2,8 @@ import drawCard
 from drawCard import drawCard
 import comTest
 from comTest import read
+from comTest import write
+from comTest import wr
 import time
 
 
@@ -26,96 +28,130 @@ def handVal(player_hand):
     
     return valSum
 
+def selectPlayerAmount():
+    write('x')
+    amount = read()
+    return amount
+
 def playerchoice(playerID, playerhand, drawCardCount, deck, MaskImgList):
     finished = False
+    if (handVal(playerhand) == 21):
+        time.sleep(0.1)
+        write('m'+ str(ord(playerID)-ord('c')))
+        time.sleep(0.1)
+        finished = True
+    else:
+        time.sleep(0.1)
+        write(playerID)
+        time.sleep(0.1)
     while (finished == False):
-        if (handVal(playerhand) == 21):
-            print("Blackjack!")
-            finished = True
-
+        
         if (finished == False):
-            print(handVal(playerhand))
-            playerInput = comTest.wr(playerID) # Function to send serial data to arduino
+            playerInput = read() # Function to send serial data to arduino
            ### playerInput = GetInpuT() # Function to get output back from arduino
             
            
 
             if playerInput == 'a':
                
-               print("Hit")
+               time.sleep(1)
                cardValue, drawCardCount = drawCard(drawCardCount, deck, MaskImgList)
+               time.sleep(1)
+               read()
                playerhand.append(cardValue)
+               
                #Send serial to arduino to dispense card
                
 
                if (handVal(playerhand) == 0):
-                   #Update score
-                   print(playerID, " Busted")
-                   finished = True
+                    #Update score
+                    write('m'+ str(ord(playerID)-ord('c')))     
+                    while(read() != "v"):
+                        time.sleep(0.01)
+                    finished = True
                     
                elif(handVal(playerhand) == 21):
-                   finished = True
+                    write('m'+ str(ord(playerID)-ord('c')))     
+                    while(read() != "v"):
+                        time.sleep(0.01)
+                    finished = True
+               else:
+                   write(playerID)
 
             elif playerInput == "b":
                #Enter bet
-               print(playerID, " Double down")
+               time.sleep(0.1)
                cardValue, drawCardCount = drawCard(drawCardCount, deck, MaskImgList)
                playerhand.append(cardValue)
+               time.sleep(0.1)
+               read()
                #Spytt ut kort
                finished = True
                
                 
                if (handVal(playerhand) == 0):
                    #Update score
-                   print("Busted")
-                   finished = True
+                    time.sleep(0.1)
+                    write('m'+ str(ord(playerID)-ord('c')))     
+                    while(read() != "v"):
+                        time.sleep(0.01)
+                    finished = True
                elif (handVal(playerhand) == 21):
-                   finished = True
-
+                    time.sleep(0.1)
+                    write('m'+ str(ord(playerID)-ord('c')))     
+                    while(read() != "v"):
+                        time.sleep(0.01)
+                    finished = True
+                   
             elif playerInput == "c":
-               print("Stand")
-               finished = True
+                time.sleep(0.1)
+                write('m'+ str(ord(playerID)-ord('c')))     
+                while(read() != "v"):
+                    time.sleep(0.01)
+                finished = True
+               
     return drawCardCount
         
 
 
 def dealerchoice(dealerhand, drawCardCount, deck, MaskImgList):
     if (handVal(dealerhand) == 0):
-        print("Dealer busted!")
         drawCardCount = drawCardCount
+        write('m'+ str(0))
         return drawCardCount
 
-    elif(handVal(dealerhand) < 16):
-        print("Hit - Dealer")
+    elif(handVal(dealerhand) < 17):
         cardValue, drawCardCount = drawCard(drawCardCount, deck, MaskImgList)
         dealerhand.append(cardValue)
+        read()
         #Spytt ut kort
         drawCardCount = dealerchoice(dealerhand, drawCardCount, deck, MaskImgList)
         return drawCardCount
         
     else:
-        print("Dealer stand")
-        print("Dealer hand: ", handVal(dealerhand))
+        write('m'+ str(0))
         return drawCardCount
 
+playerscores = [20000,20000,20000,20000,20000]
 
-def scorecompare(playerID, playerHand, dealerhand, playerscores):
+
+def scorecompare(playerID, playerHand, dealerhand):
+    time.sleep(0.5)
+    index = ord(playerID)-100
+    global playerscores
     if(handVal(playerHand) > handVal(dealerhand)):
-        print("Player ",playerID , "has a score of ", handVal(playerHand)," and beat the dealer")
-        time.sleep(0.5)
-        comTest.write(chr(ord(playerID)+5)+str(playerscores+500))
+        playerscores[index] += 50
         if(handVal(playerHand) == 21):
-            time.sleep(0.5)
-            comTest.write(chr(ord(playerID)+5)+str(playerscores+250))
+            playerscores[index] +=25
+        write(chr(ord(playerID)+5)+str(playerscores[index] ))        
+        time.sleep(2)
     elif (handVal(playerHand) == handVal(dealerhand)):
-        print("Player ",playerID ," drew")
-        time.sleep(0.5)
-        comTest.write(chr(ord(playerID)+5)+str(playerscores))
+        write(chr(ord(playerID)+5)+str(playerscores[index] ))
+        time.sleep(2)
     else:
-        print("Dealer beat player ", playerID)
-        time.sleep(0.5)
-        comTest.write(chr(ord(playerID)+5)+str(playerscores-500))
-
+        playerscores[index] -=50
+        write(chr(ord(playerID)+5)+str(playerscores[index] ))        
+        time.sleep(2)
 
 
 #     for x in range(rounds):
--- Python code/Game logic/mainGameLogic.py
+++ Python code/Game logic/mainGameLogic.py
@@ -30,12 +30,14 @@ drawCardCount = int
 drawCardCount = 0
     
 
-playerscores = [2000000,2000000,2000000,2000000]
+#playerscores = [20000,20000,20000,20000,20000]
 
+#Waits for ready signal
+read()
+time.sleep(0.1)
 
-
-
-numberOfPlayers = 1
+#Gets number of players
+numberOfPlayers = int(wr('x'))
 #send number f player to arduino
 tempID = ord('d')
 playerID = []
@@ -45,33 +47,57 @@ for i in range(numberOfPlayers):
 
 
 dealer = numberOfPlayers
-time.sleep(3)
-#write('d') 
-#print(read())
-#print(wr('d'))
+time.sleep(0.3)
+gameOn = True
+while gameOn:
+    gameOn = False
 
-while True:
     player1hand = []
     player2hand = []
     player3hand = []
     player4hand = []
     player5hand = []
+
     players = [player1hand, player2hand, player3hand, player4hand, player5hand]
+
+
     for i in range(0, 2):
         for j in range(0, numberOfPlayers + 1):
+            
+            write('m'+ str(j))
+            while(read() != "v"):
+                time.sleep(0.01)
             cardValue, drawCardCount = drawCard(drawCardCount, deck, maskImgList)
-            (players[j]).append(cardValue)
+            players[j].append(cardValue)
+            print(comTest.read())
             #send to serial to dispense card
             #sleep until card disp right position
-    print("Ready")
+
+    write('m'+ str(0))
+    while(read() != "v"):
+        time.sleep(0.01)
+        
     for i in range(numberOfPlayers):
-        
+
         drawCardCount = playerchoice(playerID[i], players[i], drawCardCount, deck, maskImgList)
-        
     drawCardCount = dealerchoice(players[dealer], drawCardCount, deck, maskImgList)
 
     for i in range(numberOfPlayers):
         
-        scorecompare(playerID[i], players[i], players[dealer], playerscores[i])
+        scorecompare(playerID[i], players[i], players[dealer])
         
     # while !Input - some input to restart gameloop. Or turn off to end game/restart
+    time.sleep(1)
+    write('q')
+    time.sleep(0.1)
+    while True:
+        dec = read()
+        if dec == 'a':
+            numberOfPlayers = int(wr('x'))
+            gameOn = True
+            break
+        elif dec == 'c':
+            gameOn = False
+            break
+
+
